78d729857ad92dabf166f8a8a6655261d04a6347
mrLuisFer <lolesuncrak@gmail.com>
2021-01-28 21:22:47 -0600

    router added in routes.ts

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,17 +1,13 @@
-import { Express } from "express"
+import { Express, Router } from "express"
 import * as controller from "./controllers/_index"
+const router: Router = Router()
 
 // Rutas
 export default function getRoutes(app: Express): void {
-  app.get("/", controller.getRoot)
-
-  app.get("/home", controller.getPots)
-
-  app.post("/home", controller.postPosts)
-
-  app.get("/article/:id", controller.getArticle)
-
-  app.delete("/article/:id", controller.deletePost)
-
-  app.put("/article/:id", controller.putPost)
+  app.use("/", router.get("/", controller.getRoot))
+  app.use("/home", router.get("/home", controller.getPots))
+  app.use("/home", router.post("/home", controller.postPosts))
+  app.use("/article/:id", router.get("/article/:id", controller.getArticle))
+  app.use("/article/:id", router.delete("/article/:id", controller.deletePost))
+  app.use("/article/:id", router.put("/article/:id", controller.putPost))
 }
