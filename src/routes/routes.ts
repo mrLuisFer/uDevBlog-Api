3e7198b4b25322fc9ef5687cc6a226a887d7e981
mrLuisFer <lolesuncrak@gmail.com>
2021-01-29 19:54:57 -0600

    fixing some code

---
+++ src/routes/routes.ts
@@ -0,0 +1,12 @@
+import { Express, Router } from "express"
+import * as controller from "./controllers/_index"
+
+// Rutas
+export default function getRoutes(app: Express): void {
+  app.get("/", controller.getRoot)
+  app.get("/home", controller.getPots)
+  app.post("/home", controller.postPosts)
+  app.get("/article/:id", controller.getArticle)
+  app.delete("/article/:id", controller.deletePost)
+  app.put("/article/:id", controller.putPost)
+}
