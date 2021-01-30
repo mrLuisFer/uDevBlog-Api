6ffa36f5b5ef539a42b96443f4804b6279b08dce
mrLuisFer <lolesuncrak@gmail.com>
2021-01-29 19:42:26 -0600

    Some deploy

--- index.ts
+++ index.ts
@@ -20,7 +20,7 @@ app.use(morgan("dev"))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static(__dirname + "/index.html"))
+app.use(express.static(__dirname + "../public/index.html"))
 
 app.listen(port, (): void => {
   console.log("- Server on port", port)
